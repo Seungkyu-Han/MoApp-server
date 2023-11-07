61847719e3e099cc53dd210f89a8edfb8cfe4f0f
trust1204@gmail.com <>
2023-11-08 00:17:26 +0900

    start

---
+++ src/test/kotlin/knu/MoApp/MoAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package knu.MoApp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
