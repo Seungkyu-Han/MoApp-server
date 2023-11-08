68d68cbcaae9708b91057272bdd12aef78c2d858
trust1204@gmail.com <>
2023-11-08 19:12:49 +0900

    [fix] Sign in 수정

---
+++ src/main/kotlin/knu/MoApp/Config/Jwt/JwtTokenProvider.kt
@@ -0,0 +1,34 @@
+package knu.MoApp.Config.Jwt
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.stereotype.Component
+import java.time.Duration
+import java.util.*
+
+@Component
+class JwtTokenProvider {
+
+    private final val accessTokenValidTime = Duration.ofDays(100).toMillis()
+
+    fun getUserId(token: String, secretKey: String): Int {
+        return Jwts.parser()
+            .setSigningKey(secretKey)
+            .parseClaimsJws(token)
+            .body
+            .get("userId", Int::class.java)
+    }
+
+    fun createAccessToken(userId: Int, secretKey: String): String{
+        val claims = Jwts.claims()
+        claims["userId"] = userId
+
+        return Jwts.builder()
+            .setHeaderParam("type", "access")
+            .setClaims(claims)
+            .setIssuedAt(Date(System.currentTimeMillis()))
+            .setExpiration(Date(System.currentTimeMillis() + accessTokenValidTime))
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact()
+    }
+}
